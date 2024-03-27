3e2659ee343be7f30b3ca89d96f36f10215ff69b
Agustin <124111465+atabisi@user.noreply.github.com>
2024-03-27 14:16:12 -0300

    Added button disabled when wrong passwords. Removed arrows from number input.

--- app.js
+++ app.js
@@ -1,13 +1,16 @@
 const firstPassword = document.querySelector('#password')
 const secondPassword = document.querySelector('#pwconfirm')
 const secondPasswordLegend = document.querySelector('#pw2legend')
+const submitButton = document.querySelector("#create")
 
 secondPassword.addEventListener('change', validatePassword)
 
 function validatePassword() {
     if (firstPassword.value !== secondPassword.value) {
         secondPasswordLegend.textContent = "Passwords do not match"
+        submitButton.disabled = true
     } else {
         secondPasswordLegend.textContent = ""
+        submitButton.disabled = false
     }
 }
